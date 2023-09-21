de656409826c5f7a91ae766302d165f7e16bf2d4
NadilaHasanah <nandinihasanah@gmail.com>
2023-09-21 12:59:36 +0700

    first commit

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model {
+    private $nama = 'Nadila';
+
+    public function getUser(){
+        return $this->nama;
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/foto.jpeg" alt="Nadila Andini" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>, yang berumur <?= $data['umur']; ?> tahun</p>
+</div>
